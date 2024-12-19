2c5fe65ae7fc403ccf5573c55a0f88f166c848d4
kzhavvv <124755214+kzhavvv@users.noreply.github.com>
2024-12-19 22:46:25 +0700

    Add files via upload

---
+++ Db/models.py
@@ -0,0 +1,8 @@
+from . import db
+
+class Items(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False, default=0)
+    price = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
